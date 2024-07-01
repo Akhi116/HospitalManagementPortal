bdec790c6771d2eb3be0f758fa417ab92eb8421e
Akhil Kumar Sadhanala <akhil.sadhanala@fedex.com>
2024-07-01 23:31:03 +0530

    patient, appointment, medical services added

---
+++ patient-service/src/test/java/com/fedex/hm/patient_service/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedex.hm.patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
